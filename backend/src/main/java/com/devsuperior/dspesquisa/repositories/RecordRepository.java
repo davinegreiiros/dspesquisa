eff86ddefc423c48c71d21bc5180aa5a7947bc42
davinegreiiros <davi.negreiiros36@gmail.com>
2020-09-15 10:09:13 -0300

    Ready for production

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -12,8 +12,8 @@ import com.devsuperior.dspesquisa.entities.Record;
 public interface RecordRepository extends JpaRepository<Record, Long>{
 
 	@Query("SELECT obj FROM Record obj WHERE" 
-	 + "(:min IS NUL OR obj.moment >= :min) AND" 
-	 + "(:max IS NULL OR obj.moment <= :max)")
+	 + "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND" 
+	 + "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
 	Page<Record> findByMoments(Instant min, Instant max, Pageable pageble);
 	
 }
