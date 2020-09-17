458e33a76dadcba429285df3d68e00549684eca9
davinegreiiros <davi.negreiiros36@gmail.com>
2020-09-17 11:22:29 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <h1>hello semana devsuperior</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
